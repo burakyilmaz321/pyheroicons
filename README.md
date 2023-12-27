52881f808c2b1dccf89e435e4087dc163803c872
burakyilmaz321 <burakyilmaz321@gmail.com>
2023-12-27 14:41:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# `pyheroicons`
+
+`pyheroicons` is a Python library that offers a convenient interface to the [Tailwind CSS Heroicons](https://heroicons.com/) icon set. This library allows developers to easily integrate Heroicons into Python-based web frameworks such as Django, Flask, or FastAPI by generating SVG icon template strings.
+
+## Installation
+
+To install `pyheroicons`, simply use pip:
+
+```bash
+pip install pyheroicons
+```
+
+## Usage
+
+Import `heroicon` function from `pyheroicons` and use it to generate icon templates:
+
+```python
+from pyheroicons import heroicon
+```
+
+## `heroicon` API
+
+- **name** The name of the Heroicon (e.g., "academic-cap").
+- **variant** The variant of the icon ("outline", "solid", "mini", or "micro").
+- **cls** Optional. A CSS class to apply to the SVG icon.
+- **strokewidth** Optional. The stroke width of the icon. <ins>Only applicable if the original SVG contains a stroke-width attribute. i.e. Only works for the "outline" variant.</ins>
+
+## Example usage
+
+```python
+icon_svg = heroicon(name="academic-cap", variant="outline", cls="icon-class", strokewidth="2")
+print(icon_svg)
+```
+
+## Keeping Up-to-Date
+
+`pyheroicons` aims to stay up-to-date with the Heroicons library. When a new version of Heroicons is released, `pyheroicons` will also release a corresponding version to ensure compatibility.
+
+| pyheroicons Version | Heroicons Version |
+| --------------------- | ----------------- |
+| 1.0.0                 | 2.1.1             |
+
+## Contributing
+
+Contributions to `pyheroicons` are welcomed!
+
+## License
+
+`pyheroicons` is released under the MIT License.
---
+++ src/pyheroicons.py
@@ -0,0 +1,79 @@
+"""
+This module provides a utility for generating SVG Heroicon templates based on provided parameters.
+"""  # noqa: E501
+
+from pathlib import Path
+from typing import Literal, get_args
+from xml.etree import ElementTree
+
+Variant = Literal["outline", "solid", "mini", "micro"]
+
+
+def heroicon(
+    name: str,
+    variant: Variant,
+    cls: str | None = None,
+    strokewidth: str | None = None,
+) -> str:
+    """
+    This function modifies the SVG file by setting the 'class' attribute and optionally
+    updating the 'stroke-width' attribute. The 'stroke-width' attribute is only modified
+    if it already exists in the SVG and a 'strokewidth' argument is provided.
+
+    Args:
+        name (str): Name of the Heroicon.
+        variant (Variant): Variant of the icon ('outline', 'solid', 'mini', 'micro').
+        cls (str, optional): Class attribute to be set in the SVG. Defaults to None.
+        strokewidth (str, optional): Value to set for the 'stroke-width' attribute. This
+                                     parameter is only used if the SVG originally contains
+                                     a 'stroke-width' attribute. Defaults to None.
+
+    Returns:
+        str: A string representation of the modified SVG.
+
+    Raises:
+        InvalidVariantError: If an invalid icon variant is provided.
+    """  # noqa: E501
+    match variant:
+        case "outline":
+            path = (
+                Path("icons") / "optimized" / "24" / "outline" / f"{name}.svg"
+            )
+        case "solid":
+            path = Path("icons") / "optimized" / "24" / "solid" / f"{name}.svg"
+        case "mini":
+            path = Path("icons") / "optimized" / "20" / "solid" / f"{name}.svg"
+        case "micro":
+            path = Path("icons") / "optimized" / "16" / "solid" / f"{name}.svg"
+        case _:
+            raise InvalidVariantError
+
+    # Parse SVG file
+    ElementTree.register_namespace("", "http://www.w3.org/2000/svg")
+    tree = ElementTree.parse(path)
+    root = tree.getroot()
+
+    # Set or modify the 'class' attribute
+    if cls is not None:
+        root.set("class", cls)
+
+    # Check if 'stroke-width' exists, then set or modify it
+    if strokewidth is not None:
+        if root.get("stroke-width"):
+            root.set("stroke-width", strokewidth)
+
+    return ElementTree.tostring(root, encoding="unicode")
+
+
+class InvalidVariantError(Exception):
+    """
+    Custom exception raised when an invalid icon variant is provided.
+    """
+
+    def __init__(self):
+        """
+        Initialize the exception.
+        """
+        valid_options = ", ".join(get_args(Variant))
+        message = f"Invalid variant. Expected one of: {valid_options}."
+        super().__init__(message)
